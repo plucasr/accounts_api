b9179d3bc0ea95164f8b617c695626989b549456
Lucas Pedroso <webdevlucas@gmail.com>
2025-03-18 22:10:01 -0300

    feat(contacts): user / contacts setup adjusts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+
+
+
+5.times do
+  user = User.create!(
+    email: Faker::Internet.unique.email,
+    password: "password123",
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
+  )
+
+  # Create 2 Contacts for each User
+  2.times do
+    Contact.create!(
+      slug: [ "email", "phone" ].sample,
+      label: Faker::Lorem.word,
+      value: Faker::Internet.email, # or Faker::PhoneNumber.phone_number
+      user: user
+    )
+  end
+end
+
+puts "Seeds created successfully!"
